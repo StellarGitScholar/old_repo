62d7b02e4ca4937cbe9ec2561f3faedc9a3d4012
StellarGitScholar <dumbocollenzo@gmail.com>
2023-02-20 20:03:59 -0800

    print lower case alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* my header goes there */
+
+/**
+ * main - A code that prints lowercase alphabet
+ * Return: Always 0
+ * */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char az;
+
+	for (az = 'a' ; az <= 'z' ; az++)
+	{
+		putchar (az);
+	}
+/* my code goes there */
+	return (0);
+}
