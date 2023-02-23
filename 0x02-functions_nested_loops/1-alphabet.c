2af14704d2d673e2e0d621af0a4bbe4b5786d1d2
StellarGitScholar <dumbocollenzo@gmail.com>
2023-02-23 05:40:46 -0800

    print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,16 +3,12 @@
 /**
  * print_alphabet - print all alphabet in lowercase
  */
-int main(void)
+void print_alphabet(void);
 {
-	void print_alphabet(void);
-	{
-		char letter;
+	char letter;
 
-		for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
+	for (letter = 'a'; letter <= 'z'; letter++)
+	_putchar(letter);
 
-		_putchar('\n');
-	}
-	return 0;
+	_putchar('\n');
 }
