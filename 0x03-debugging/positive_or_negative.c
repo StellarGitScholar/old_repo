d15917de844f0c115f46e54dd6861c27d00bade8
StellarGitScholar <dumbocollenzo@gmail.com>
2023-02-24 07:49:40 -0800

    debugging task0

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -2,33 +2,18 @@
 #include <time.h>
 #include <stdlib.h>
 
-
-/**
- *main - A program that checks if a given number
- *	is positive or  negative
- *Return: Always 0
- */
 void positive_or_negative(int i)
 {
-
-	i = 0;
-
 	if (i > 0)
 	{
 		printf("%d is positive\n", i);
 	}
+	else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
 	else
 	{
-		if (i < 0)
-		{
-			printf("%d is negative\n", i);
-		}
-		else
-		{
-			if (i == 0)
-			{
-				printf("%d is zero\n", i);
-			}
-		}
+		printf("%d is negative\n", i);
 	}
 }
