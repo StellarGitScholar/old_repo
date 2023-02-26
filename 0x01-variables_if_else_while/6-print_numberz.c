827580ab498383a37ecb3a6e6f9a92a6e5b25b6d
StellarGitScholar <dumbocollenzo@gmail.com>
2023-02-26 07:16:20 -0800

    retry

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints numbers 0-9
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0' ; n <= '9' ; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	/* code to print numbers */
+
+	return (0);
+}
