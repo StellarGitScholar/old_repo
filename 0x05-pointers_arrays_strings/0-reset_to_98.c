2dfcc4f5cc1efadc47624c9e8c244cd1b0c6687b
StellarGitScholar <dumbocollenzo@gmail.com>
2023-02-27 15:26:03 -0800

    commit for task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,11 @@
 #include "main.h"
 /**
- * Desc: This is a function that updates the value of the int
+ * reset_to_98 - This is a function that updates the value of the int
  *	it points to to 98
+ *@n: pointer to be modified
+ *Return: Always 0
  */
 void reset_to_98(int *n)
 {
-	*n=98;
+	*n = 98;
 }
