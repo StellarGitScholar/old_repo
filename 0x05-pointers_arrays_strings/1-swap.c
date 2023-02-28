0821c239c5ea0f403f633112464aa075ca8b0825
StellarGitScholar <dumbocollenzo@gmail.com>
2023-02-27 16:14:30 -0800

    commit for task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * swap_int - This is a function that swaps the value of two integers
+ *@a: integer to be modified
+ *@b: integer to be modified
+ */
+void swap_int(int *a, int *b);
+{
+	*a = 42;
+	*b = 98;
+}
