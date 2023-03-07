2d4f6afc8557f79250a4e396a452cb7dfdb3164b
StellarGitScholar <dumbocollenzo@gmail.com>
2023-03-07 13:53:04 -0800

    commit for task 0

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory is for my more pointers, arrays and strings project. I am yet to create a good enough readme file.
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: mmain
+ *
+ * Description/Type: header file
+ */
+char *_memset(char *s, char b, unsigned int n);
+#endif
