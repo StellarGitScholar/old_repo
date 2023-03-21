d212bf7b7d22f5ae0d04043c79f7019df2fa9bb9
StellarGitScholar <dumbocollenzo@gmail.com>
2023-03-21 09:20:02 -0700

    correction commit for the advanced task of the argc_argv project

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -16,26 +16,34 @@ int main(int argc, char *argv[])
 	int cents, coins = 0;
 
 	if (argc == 2)
+	{
 		cents = atoi(*(argv + 1));
 		while (cents > 0)
+		{
 			if (cents % 25 < cents)
+			{
 				cents -= 25;
-				coins++;
+				coins++; }
 			else if (cents % 10 < cents)
+			{
 				cents -= 10;
-				coins++;
+				coins++; }
 			else if (cents % 5 < cents)
+			{
 				cents -= 5;
-				coins++;
+				coins++; }
 			else if (cents % 2 < cents)
+			{
 				cents -= 2;
-				coins++;
+				coins++; }
 			else if (cents % 1 < cents)
+			{
 				cents -= 1;
-				coins++;
-	else
+				coins++; } }
+	} else
+		{
 		printf("Error\n");
-		return (1);
+		return (1); }
 	printf("%d\n", coins);
 	return (0);
 }
