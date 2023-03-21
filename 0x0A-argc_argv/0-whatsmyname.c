c312d222b43fef3c1f266faa16181941c861c4aa
StellarGitScholar <dumbocollenzo@gmail.com>
2023-03-21 08:00:53 -0700

    initial commit for task 0 of the argc_argv project

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - a function that prints the name of the file
+ * @argc: is an argument counter
+ * @argv: is the argument value
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - argc]);
+	return (0);
+}
