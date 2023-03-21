0fd4beb47f694dec8a2d2ec76faa0503eea4e380
StellarGitScholar <dumbocollenzo@gmail.com>
2023-03-21 02:00:02 -0700

    Initial commit for task 4 preprocessor project

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,14 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ *Sum - A header file that sums two numbers
+ *
+ *@x: variable to be summed
+ *
+ *@y: variable to be summed
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif /* MAIN_H */
