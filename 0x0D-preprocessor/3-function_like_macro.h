9c08fd69af65bfc609bbae04cc03802ca017f69c
StellarGitScholar <dumbocollenzo@gmail.com>
2023-03-21 01:47:13 -0700

    final commit for task 3 preprocessor project

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef ABS_H
+#define ABS_H
+
+/**
+ *Abs - this is a header file with a function to print absolute
+ *	value of x
+ *@x: value to print the absolute of
+ */
+
+#define ABS(x) ((x) < 0 ? (-(x)) : (x))
+
+#endif /* ABS_H */
