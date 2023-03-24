49346125ec11a7b7a0175db5f5edf29073e4196e
StellarGitScholar <dumbocollenzo@gmail.com>
2023-03-23 19:32:22 -0700

    commit for task 0 of the variadic project

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,14 @@
+#ifndef VARIADIC_H
+#define VARIADIC_H
+
+/**
+ *description/type - Header file
+ *
+ *@n: parameter
+ *
+ *Return: 0
+ */
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif /* VARIADIC_H */
