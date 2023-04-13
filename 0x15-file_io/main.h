7b2140fc8e1280686b946afb1faa2513ea070d18
StellarGitScholar <dumbocollenzo@gmail.com>
2023-04-13 16:14:03 -0700

    commit for task 1 of the file io project

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stddef.h>
+
 /**
  *read_textfile - reads a text file and prints the letters
  *
@@ -16,5 +17,6 @@
  */
 
 size_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif /*MAIN_H*/
